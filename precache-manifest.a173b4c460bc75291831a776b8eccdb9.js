fdc5e07e2cda0e92ba388bdb659642ddfed75f70
Jeremy Noh <jeremy@wertkt.com>
2019-01-06 16:05:17 +0100

    Updates

---
+++ precache-manifest.a173b4c460bc75291831a776b8eccdb9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c41542e07648518c6c6e",
+    "url": "./build/static/js/runtime~main.c41542e0.js"
+  },
+  {
+    "revision": "4b4c98448caa1fc33aee",
+    "url": "./build/static/js/main.4b4c9844.chunk.js"
+  },
+  {
+    "revision": "916457b81fbfb4acadc8",
+    "url": "./build/static/js/1.916457b8.chunk.js"
+  },
+  {
+    "revision": "4b4c98448caa1fc33aee",
+    "url": "./build/static/css/main.8026807a.chunk.css"
+  },
+  {
+    "revision": "83e9e16d18ffaeac84947a6255f62cef",
+    "url": "./build/index.html"
+  }
+];
