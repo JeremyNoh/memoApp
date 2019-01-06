75e8e9dc170e589fe3be6e536297c639fbc791fe
Jeremy Noh <jeremy@wertkt.com>
2019-01-06 14:42:53 +0100

    add build prod for test

---
+++ build/precache-manifest.3f6f077f36ea7e1c6c7fa83de72f1a8c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8dd91205e0d72f81ec3b",
+    "url": "/myapp/static/js/runtime~main.8dd91205.js"
+  },
+  {
+    "revision": "d9c7a9408c0dc286b947",
+    "url": "/myapp/static/js/main.d9c7a940.chunk.js"
+  },
+  {
+    "revision": "b378cc44e89d39be222e",
+    "url": "/myapp/static/js/1.b378cc44.chunk.js"
+  },
+  {
+    "revision": "d9c7a9408c0dc286b947",
+    "url": "/myapp/static/css/main.8f0003f7.chunk.css"
+  },
+  {
+    "revision": "d1c8ef779853daf2ae1891bfd06c703b",
+    "url": "/myapp/index.html"
+  }
+];
