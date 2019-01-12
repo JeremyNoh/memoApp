3f23c544e0ae10c62b1c3904bbe862a2ac1a666f
Jeremy Noh <jeremy@wertkt.com>
2019-01-12 19:22:56 +0100

    Updates

---
+++ precache-manifest.b7756204ea3eec5ee5d1dafcbdec6274.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "25755c5fc8531f4bb2a3",
+    "url": "/memoApp/static/js/runtime~main.25755c5f.js"
+  },
+  {
+    "revision": "2aef66d87c2604c3fae7",
+    "url": "/memoApp/static/js/main.2aef66d8.chunk.js"
+  },
+  {
+    "revision": "a994efe6977218d8f95a",
+    "url": "/memoApp/static/js/1.a994efe6.chunk.js"
+  },
+  {
+    "revision": "2aef66d87c2604c3fae7",
+    "url": "/memoApp/static/css/main.5f911fbf.chunk.css"
+  },
+  {
+    "revision": "095afb71b8523bdca8dc214c4972bf97",
+    "url": "/memoApp/index.html"
+  }
+];
